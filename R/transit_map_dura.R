499e22e8cf374b7ceea423b2020b431d45ee094a
Keigo Mastuo <m.keigo.sp.ku@gmail.com>
2018-05-06 22:32:32 +0900

    add

---
+++ R/transit_map_dura.R
@@ -0,0 +1,30 @@
+#' mapping duration
+#'
+#' @param orlon map center longitude
+#' @param orlat map center latitude
+#' @param zoom ggmap zoom
+#' @param data data.frame including longitude,latitude,fare
+#'
+#' @importFrom ggmap get_googlemap
+#' @importFrom ggmap ggmap
+#' @importFrom ggplot2 geom_point
+#' @importFrom ggplot2 scale_color_gradient
+#'
+#' @examples
+#' \dontrun{
+#' # center:Kobe University
+#' transit.map.dura(135.235395,34.725918,11,Data)
+#' }
+#'
+#' @export
+#'
+#' @author Keigo Matsuo \email{m.keigo.sp.ku@@gmail.com}
+#'
+transit_map_dura <- function(orlon, orlat, zoom, data){
+  library(ggmap)
+  nyc_map <- get_googlemap(center = c(lon =orlon, lat = orlat),
+                           zoom = zoom, scale = 2, color="bw" )
+  ggmap(nyc_map, legend="none") +
+    geom_point(aes(longitude, latitude, color= duration),data = data)+
+    scale_color_gradient(low="yellow",high="red")
+}
