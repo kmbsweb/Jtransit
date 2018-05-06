503abd0c427da41843418b316318733e0c382c6b
Keigo Mastuo <m.keigo.sp.ku@gmail.com>
2018-05-06 20:30:16 +0900

    add

--- R/transit_map_dura.R
+++ R/transit_map_dura.R
@@ -1,4 +1,4 @@
-#' mapping fare
+#' mapping duration
 #'
 #' @param orlon map center longitude
 #' @param orlat map center latitude
@@ -13,19 +13,18 @@
 #' @examples
 #' \dontrun{
 #' # center:Kobe University
-#' transit.map.fare(135.235395,34.725918,11,Data)
+#' transit.map.dura(135.235395,34.725918,11,Data)
 #' }
 #'
 #' @export
 #'
 #' @author Keigo Matsuo \email{m.keigo.sp.ku@@gmail.com}
 #'
-transit_map_fare <- function(orlon,orlat,zoom,data){
+transit_map_dura <- function(orlon, orlat, zoom, data){
   library(ggmap)
   nyc_map <- get_googlemap(center = c(lon =orlon, lat = orlat),
                            zoom = zoom, scale = 2, color="bw" )
   ggmap(nyc_map, legend="none") +
-    geom_point(aes(longitude, latitude, color= fare),data = data)+
+    geom_point(aes(longitude, latitude, color= duration),data = data)+
     scale_color_gradient(low="yellow",high="red")
 }
-
